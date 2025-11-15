90d6f1aa826a0bb4a34862ba1c5574aae8d6b31b
Meigi09 <rotimeigi32@gmail.com>
2025-11-15 14:10:59 +0200

    third commit

--- python-context-async-perations-0x02/0-databaseconnection.py
+++ python-context-async-perations-0x02/0-databaseconnection.py
@@ -5,19 +5,27 @@ import sqlite3
 class DatabaseConnection:
     """Custom class-based context manager for DB connection."""
 
+    def __init__(self, db_name="users.db"):
+        """Store the database name."""
+        self.db_name = db_name
+        self.conn = None
+        self.cursor = None
+
     def __enter__(self):
-        self.conn = sqlite3.connect("users.db")
+        """Open the database connection and return the cursor."""
+        self.conn = sqlite3.connect(self.db_name)
         self.cursor = self.conn.cursor()
         return self.cursor
 
     def __exit__(self, exc_type, exc_value, traceback):
+        """Close the connection automatically."""
         if self.conn:
             self.conn.close()
-        # Do not suppress exceptions
+        # do not suppress exceptions
         return False
 
 
-# Use the context manager to run SELECT * FROM users
+# Using the context manager to run SELECT * FROM users
 if __name__ == "__main__":
     with DatabaseConnection() as cursor:
         cursor.execute("SELECT * FROM users")
